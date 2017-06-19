70f6ed4f4df8a9d9d6579e85a2773c111100a207
nalsadi <nalsadi01@gmail.com>
2017-06-19 16:50:51 -0400

    Detect How Happy And Import Kivy

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import kivy
 import sys
-#import google.cloud import vision
+import io
+from google.cloud import vision
 
-#var = vision.Client()
+var = vision.Client()
+
 print "Arguemnets Passed In: ",str(sys.argv)
+
+def action_type(args):
+    if(args[1] == "Exp?"):
+        facial_values(args[2])
+
+def facial_values(path):
+    with io.open(path,'rb') as imageFile:
+        imagePassing = imageFile.read()
+    image = var.image(imagePassing)
+
+    faces = image.detect_faces()
+    for face in faces:
+        print "Amount of Joy",face.emotions.joy
+        print
+
+
+
+
+action_type(sys.argv)
